8de0820dabe380cdd2a1d2943055c69a21e21d37
makafis <vinnyalfieri@gmail.com>
2015-06-11 17:19:57 -0400

    complete

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    return_array = []
+        self.each do |animal, place|
+ 
+            arguments.each do |value_to_match|
+              #binding.pry 
+
+                if value_to_match == place
+                  return_array << animal
+                end
+
+            end
+
+        end
+
+    return_array
+
+
+
   end
 end
